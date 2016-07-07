a67d6fa04dc471a9eb1845975c887109da9b252e
Christian Whitesides <whitesidescr@gmail.com>
2016-07-07 12:20:21 -0600

    Create Application, Users, and Books controllers

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+require './config/environment'
+require 'rack-flash'
+
+class ApplicationController < Sinatra::Base
+  use Rack::Flash
+  
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do 
+    erb :index
+  end
+
+end
