06ee6db1787c378e154cbdda2bb9b6bdbd55b73c
Christian Whitesides <whitesidescr@gmail.com>
2016-07-07 15:06:05 -0600

    Add controller actions to create books and create_book view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,4 +9,21 @@ class BooksController < ApplicationController
       redirect to '/login'
     end
   end
+
+  get '/books/new' do
+    erb :'/books/create_book'
+  end
+
+  post '/books' do
+    @book = Book.new(params)
+
+    if @book.title == "" || @book.author == ""
+      flash[:message] = "A book just isn't a book without a title and an author..."
+      redirect to '/books/new'
+    else
+      @user = User.find(session[:user_id])
+      @user.books << @book
+      redirect to "/books/#{@book.id}"
+    end
+  end
 end
