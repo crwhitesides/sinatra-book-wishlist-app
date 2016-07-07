0fa0887d6bf33a2b2df198fcf02b4e6042bf6182
Christian Whitesides <whitesidescr@gmail.com>
2016-07-07 14:53:18 -0600

    Add conditional to /books controller action so that only account holders can access book index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,12 @@
 class BooksController < ApplicationController
 
   get '/books' do
-    @user = User.find(session[:user_id])
-    @books = @user.books
-    erb :'/books/books'
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      @books = @user.books
+      erb :'/books/books'
+    else
+      redirect to '/login'
+    end
   end
 end
