f2931f886a2588c6fcf9729a29af7d7a00ae76ea
Christian Whitesides <whitesidescr@gmail.com>
2016-07-07 12:22:13 -0600

    Enable sessions in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "book_wishlist_secret"
   end
 
   get '/' do 
