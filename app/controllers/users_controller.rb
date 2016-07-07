cabbb4b1d41ec898cdcf9cb47b2ec9f9a90ae087
Christian Whitesides <whitesidescr@gmail.com>
2016-07-07 14:48:59 -0600

    Add /login controller actions and create login view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,28 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if session[:user_id]
+      redirect to '/books'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username]) 
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/books'
+    elsif params[:username] == "" || params[:password] == ""
+      flash[:message] = "Oops! Don't forget to fill in both fields."
+      redirect to '/login'
+    else
+      flash[:message] = "It looks like you don't have an account. Sign up now!"
+      redirect to '/signup'
+    end
+  end
+
   get '/logout' do
     if session[:user_id]
       session.clear
